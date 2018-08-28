cb429efd30afbbcf3079cf369e17f103627c788f
chup1m <42609668+chup1m@users.noreply.github.com>
2018-08-28 09:39:10 -0300

    M  inc/slug.php

--- inc/slug.php
+++ inc/slug.php
@@ -5,10 +5,8 @@
 function slug($text,$set=true){
     if($set){
         $text=str_replace(' ', '_', $text);
-        $text=urlencode($text);
     }else{
         $text=str_replace('_', ' ', $text);
-        $text=urldecode($text);
     }
     return $text;
 }
